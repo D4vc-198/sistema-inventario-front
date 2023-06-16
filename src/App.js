b401b2ecd1a7835915b1cef9693587ec6caf9a26
Oscar Guerra <oogl29@gmail.com>
2023-06-16 15:26:07 -0500

    Create the componets cards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import Tabla from './Components/Tabla/Tabla';
 import Tarjeta from './Components/Tarjetas/Tarjeta';
+import { BsFillCalendarEventFill, BsFillStarFill, BsFillClipboard2CheckFill, BsCalculatorFill } from "react-icons/bs";
 
 function App() {
   return (
     <>
       <Tabla />
-      <Tarjeta />
+      <div className="menu-lista-tarjetas">
+      <Tarjeta icon={ <BsFillCalendarEventFill size="2em"/>} cardTitle={"Expiration"} cardDescription={"0,0"}/>
+      <Tarjeta icon={ <BsFillStarFill size="2em"/>} cardTitle={"Bestseller"} cardDescription={"0,0"}/>
+      <Tarjeta icon={ <BsFillClipboard2CheckFill size="2em"/>} cardTitle={"Low inventory"} cardDescription={"0,0"}/>
+      <Tarjeta icon={ <BsCalculatorFill size="2em"/>} cardTitle={"Accounting"} cardDescription={"0,0"}/>
+      </div>
     </>
-  )
+  );
 }
 
 export default App;
