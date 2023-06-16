df42d72a319ecb14a9ef22f2f4e8428b41c3ffde
Javier Icaza Santos <javiicazaturn@Javiers-MacBook-Pro.local>
2023-06-16 16:03:34 -0600

    table skeleton

--- src/App.js
+++ src/App.js
@@ -3,9 +3,44 @@ import Tabla from './Components/Tabla/Tabla';
 import Tarjeta from './Components/Tarjetas/Tarjeta';
 
 function App() {
+  const headerCell = [{name: "Employee"}, {name: "Product"}, {name: "SKU"}, {name: "Expiry Day"}, {name: "Quantity"}, {name: "Price"}]
+  const tableData = [
+    {
+      Employee: "Pedro", 
+      Product: "Producto1",
+      SKU: "16890002FR",
+      ExpiryDay: "MM/DD/YYYY",
+      Quantity: "12",
+      Price: "$212",
+    }, 
+    {
+      Employee: "Juan", 
+      Product: "Producto2",
+      SKU: "16890014FR",
+      ExpiryDay: "MM/DD/YYYY",
+      Quantity: "12",
+      Price: "$512",
+    }, 
+    {
+      Employee: "Oscar", 
+      Product: "Producto3",
+      SKU: "16890012FR",
+      ExpiryDay: "MM/DD/YYYY",
+      Quantity: "2",
+      Price: "$22",
+    }, 
+    {
+      Employee: "Alsonso", 
+      Product: "Producto4",
+      SKU: "1689888112FR",
+      ExpiryDay: "MM/DD/YYYY",
+      Quantity: "21",
+      Price: "$752",
+    }, 
+  ]
   return (
     <>
-      <Tabla />
+      <Tabla header={headerCell} data={tableData}/>
       <Tarjeta />
     </>
   )
--- src/Components/Tabla/Tabla.js
+++ src/Components/Tabla/Tabla.js
@@ -1,10 +1,30 @@
 import React from "react"
+import "./../../Styles/Tables.css"
 
-const Tabla = () => {
+const Tabla = (props) => {
+    const { header, data} = props 
+    console.log(data)
     return (
-        <>
-            <p>Tabla</p>
-        </>
+        <table>
+        <thead>
+        <tr className="inventory-table">
+           {header.map((header) => <th key={header.name}>{header.name}</th>)}
+        </tr>    
+        </thead> 
+        <tbody>
+        {data.map((information) => (
+        <tr key={information.Employee}>
+            <td className="table-cell">{information.Employee}</td>
+            <td className="table-cell">{information.Product}</td>
+            <td className="table-cell">{information.SKU}</td>
+            <td className="table-cell">{information.ExpiryDate}</td>
+            <td className="table-cell">{information.Quantity}</td>
+            <td className="table-cell">{information.Price}</td>
+        </tr>
+        ))}
+        </tbody>
+        
+        </table>
     )
 }
 
